069daf2de1cf393e7209b1e2f9fab333ce81975f
João Pedro Kunrath <88558548+rasquinhaz@users.noreply.github.com>
2021-08-31 14:51:27 -0300

    Update ex060.py

--- ex060.py
+++ ex060.py
@@ -1,2 +1,10 @@
+from math import factorial
+print('Digite um número para calcular o seu FATORIAL.')
+n = int(input('Seu número: '))
+c = n
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end=' ')
+    c -= 1
 
 
